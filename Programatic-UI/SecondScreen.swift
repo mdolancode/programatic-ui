2d2854ccb2855acaf3a57299936933770e35b099
Matthew Dolan <mdolancode@gmail.com>
2023-01-30 12:28:41 -0800

    Add second screen.

---
+++ Programatic-UI/SecondScreen.swift
@@ -0,0 +1,16 @@
+//
+//  SecondScreen.swift
+//  Programatic-UI
+//
+//  Created by Matthew Dolan on 2023-01-30.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+}
